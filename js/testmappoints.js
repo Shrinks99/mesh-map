0733cfd6c7be19ee651088f61c4cc80b09ebfb86
Henry Wilkinson <5672810+Shrinks99@users.noreply.github.com>
2019-04-23 01:44:17 -0400

    Config Fixes

--- js/testmappoints.js
+++ js/testmappoints.js
@@ -2,20 +2,23 @@ markers = [
   {
     "name": "Node 1",
     "lat": 43.6518,
-    "lng": -79.3817
+    "lng": -79.3817,
+    "online": 1
   },
   {
     "name": "Node 2",
     "lat": 43.6620,
-    "lng": -79.3720
+    "lng": -79.3720,
+    "online": 0
   }
 ];
 
-////Marker placement & Icon (doesn't seem to like being loaded after the markers FYI)
-var myIcon = L.divIcon({className: 'onlinenode'});
+////Marker placement & Icon (doesn't seem to like being loaded before the markers FYI)
+var onlineNode = L.divIcon({className: 'onlinenode'});
+var offlineNode = L.divIcon({className: 'offlinenode'});
 
     for (var i = 0; i < markers.length; ++i) {
-      L.marker([markers[i].lat, markers[i].lng], {icon: myIcon})
+      L.marker([markers[i].lat, markers[i].lng], {icon: onlineNode})
         .bindPopup('<h3>' + markers[i].name + '</h3>')
         .addTo(map);
     }
