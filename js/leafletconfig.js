7084c899187ac15772af8e9e3ab005e9ab8fcedc
Henry Wilkinson <5672810+Shrinks99@users.noreply.github.com>
2020-08-17 00:46:31 -0400

    Leaflet Map Data overhaul

--- js/leafletconfig.js
+++ js/leafletconfig.js
@@ -1,12 +1,41 @@
-//General map config
+// General map position & UI config
 var map = L.map('map', {
   center: [43.651890, -79.381706],
-  minZoom: 11,
-  zoom: 14,
+  // minZoom: 11,
+  zoom: 13,
+  zoomControl: false
 });
 
-//Map tile layer setup
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, tiles &copy; <a href="https://carto.com/attributions">CARTO</a>',
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(map);
+
+// Map tile layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, Map Tiles &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
+}).addTo(map);
+
+// Points layer
+$.getJSON("../nodelist.geo.json",function(data){
+
+  var nodeIcon = L.divIcon({
+    iconSize: new L.Point(15,15),
+    className: 'onlinenode'
+  });
+
+ var geoJsonLayer = L.geoJson(data,{
+   pointToLayer: function (feature, latlng) {
+     return L.marker(latlng, {icon: nodeIcon});
+   }
+ }).addTo(map);
+});
+
+// Node Info layer
+var nodeinfo = L.geoJson({
+  pointToLayer: function(feature, latlng){
+    var marker = L.marker(latlng, {icon:nodeIcon});
+    marker.bindPopup(features.properties.name + '<br/>' + features.properties.type);
+    return marker;
+  }
 }).addTo(map);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Mesh map
+
+This is an experimental test of Leaflet and OpenStreetMap for mapping a network of mesh nodes in a city.  It pulls data from a single JSON file formatted according to the [geoJSON spec](https://geojson.org/)
+
+## Development
+
+You must be running a web server on your machine or else this probably won't work properly.  I personally like [http-server](https://github.com/http-party/http-server) but use whatever works for you <3
